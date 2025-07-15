6c9c549451ec5ddd1ee16ed50b7943f63b3d6421
BBB666th <boyd177@gmail.com>
2025-07-15 21:52:52 +0700

    Fix: import cloudinary

--- runner.py
+++ runner.py
@@ -1,18 +1,11 @@
+import cloudinary
 from app import capture_and_send
-capture_and_send()
-from dotenv import load_dotenv
-load_dotenv()
 
-import os
 cloudinary.config(
-    cloud_name=os.getenv("CLOUDINARY_CLOUD_NAME"),
-    api_key=os.getenv("CLOUDINARY_API_KEY"),
-    api_secret=os.getenv("CLOUDINARY_API_SECRET")
+    cloud_name = os.getenv("CLOUDINARY_CLOUD_NAME"),
+    api_key = os.getenv("CLOUDINARY_API_KEY"),
+    api_secret = os.getenv("CLOUDINARY_API_SECRET")
 )
 
 if __name__ == "__main__":
     capture_and_send()
-
-
-
-
